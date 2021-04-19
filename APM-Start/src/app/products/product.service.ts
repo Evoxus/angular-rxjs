f59cd55e1cd8341530ef2abc479ab7cba7221cbc
Michael Ploughman <michael.ploughman@blndspt.com>
2021-04-19 13:23:00 -0600

    Mapping Returned Data

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -17,10 +17,17 @@ export class ProductService {
 
   products$ = this.http.get<Product[]>(this.productsUrl)
   .pipe(
+    map(productArr => 
+      productArr.map(item => ({
+        ...item,
+        price: item.price * 3,
+        searchKey: [item.productName]
+        }) as Product )
+    ),
     tap(data => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)
   );
-  
+
   constructor(private http: HttpClient,
               private supplierService: SupplierService) { }
 
